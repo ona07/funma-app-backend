1381f44c19740786e066ee9a88da622198229000
小久保尚 <kokubonao@kokubonaonoMacBook-Air.local>
2025-03-04 16:20:11 +0900

    cors

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# CORS の設定
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # すべてのオリジンを許可（制限する場合は ["http://192.168.1.9:3000"]）
+    allow_credentials=True,
+    allow_methods=["*"],  # GET, POST などをすべて許可
+    allow_headers=["*"],  # すべてのヘッダーを許可
+)
 
 @app.get("/hello")
-async def hello():
+def hello():
     return {"message": "hello world!"}
